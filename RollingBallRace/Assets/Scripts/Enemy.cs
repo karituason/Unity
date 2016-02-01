7d53d24f04d81cffc2d254d63646810d23abfe2f
karituason <karituason@gmail.com>
2016-01-31 21:51:55 -0500

    first commit

---
+++ RollingBallRace/Assets/Scripts/Enemy.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour {
+	public float speed;
+	public Text winText;
+	public Text enemyText;
+
+	private Rigidbody rb;
+	private GameObject[] pickups;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		pickups = GameObject.FindGameObjectsWithTag ("Pickup");
+		count = 0;
+		setCountText ();
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		GameObject p = findClosest ();
+		if (p != null) {
+			float moveHorizontal = p.transform.position.x - this.gameObject.transform.position.x;
+			float moveVertical = p.transform.position.z - this.gameObject.transform.position.z;
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			rb.AddForce (movement * speed);
+		} else if (count == 6) {
+			winText.text = "It's a tie!";
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive (false);
+		}
+		count++;
+		setCountText ();
+	}
+
+	GameObject findClosest(){
+		GameObject min = null;
+		float minDist = Mathf.Infinity;
+		foreach (GameObject t in pickups) {
+			if (t.activeSelf) {
+				float dist = Vector3.Distance (t.transform.position, transform.position);
+				if (dist < minDist) {
+					min = t;
+					minDist = dist;
+				}
+			}
+		}
+		return min;
+	}
+
+	void setCountText(){
+		enemyText.text = "Enemy Count: " + count.ToString ();
+		if (count >= 7) {
+			winText.text = "You Lose!";
+		}
+	}
+}
---
+++ RollingBallRace/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setCountText ();
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText ();
+		}
+	}
+
+	void setCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 7) {
+			winText.text = "You Win!";
+		}
+	}
+}
+//Destroy (other.gameObject);
